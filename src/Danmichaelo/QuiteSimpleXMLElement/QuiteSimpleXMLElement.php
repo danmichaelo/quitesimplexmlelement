f043ba66ab1063171311afb19171b1756597d188
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-07-01 21:35:18 +0200

    Extract HelperMethods

--- src/Danmichaelo/QuiteSimpleXMLElement/QuiteSimpleXMLElement.php
+++ src/Danmichaelo/QuiteSimpleXMLElement/QuiteSimpleXMLElement.php
@@ -48,6 +48,8 @@ use SimpleXMLElement;
 
 class QuiteSimpleXMLElement
 {
+    use HelperMethods;
+
     /**
      * @var array
      */
@@ -122,7 +124,7 @@ class QuiteSimpleXMLElement
     /**
      * Internal helper method to parse content from string.
      *
-     * @param object $content
+     * @param $elem
      * @return SimpleXMLElement
      */
     private function initFromObject($elem)
@@ -177,44 +179,6 @@ class QuiteSimpleXMLElement
     }
 
     /**
-     * Get a node attribute value.
-     *
-     * @param string $attribute
-     * @return string
-     */
-    public function attr($attribute)
-    {
-        return trim((string) $this->el->attributes()->{$attribute});
-    }
-
-    /**
-     * Get the first node matching an XPath query, or null if no match.
-     *
-     * @param string $path
-     * @return QuiteSimpleXMLElement
-     */
-    public function first($path)
-    {
-        // Convenience method
-        $x = $this->xpath($path);
-
-        return count($x) ? $x[0] : null;
-    }
-
-    /**
-     * Check if the document has at least one node matching an XPath query.
-     *
-     * @param string $path
-     * @return bool
-     */
-    public function has($path)
-    {
-        $x = $this->xpath($path);
-
-        return count($x) ? true : false;
-    }
-
-    /**
      * Get the wrapped SimpleXMLElement object.
      *
      * @return SimpleXMLElement
@@ -222,30 +186,6 @@ class QuiteSimpleXMLElement
     public function el()
     {
         return $this->el;
-    }
-
-    /**
-     * Get all nodes matching an XPath query.
-     *
-     * @param string $path
-     * @return QuiteSimpleXMLElement[]
-     */
-    public function xpath($path)
-    {
-        return array_map(function ($el) {
-            return new QuiteSimpleXMLElement($el, $this);
-        }, $this->el->xpath($path));
-    }
-
-    /**
-     * Alias for `xpath()`.
-     *
-     * @param $path
-     * @return QuiteSimpleXMLElement[]
-     */
-    public function all($path)
-    {
-        return $this->xpath($path);
     }
 
     /**
